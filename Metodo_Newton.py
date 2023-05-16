ab914a39610d311801ea8d666b12cbf53c54434f
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-15 19:36:00 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -6,5 +6,11 @@ import numpy as np
 
 st.header(":blue[Metodo de Newton(-Raphson]")
 """
-El metodo de Newton es una alternativa del metodod del punto fijo ya que este consiste en obtener ecuaciones $g_i$
+El metodo de Newton es una alternativa del metodo del punto 
+fijo ya que la gran desventaja de este se consiste en obtener ecuaciones $g_i$ que converja 
+en el intervalo de solucion y dicha búsqueda puede representar un esfuerzo de cálculo considerable, en lo unico en que se parecen en estos dos metodos es 
+la generalización multivariable del método de Newtoon para una sola variable.
+
+Este metodo solo sirve para los sistemas de ecuaciones no lineales, este requiere algunos conceptos previos, los cuales se presentan a continuación.
+
 """
