820678049e9c16ba77c3050c63201bb4c65a4daf
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-16 12:37:32 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -63,11 +63,11 @@ $f(x_{i+1})$ por definición es igual a cero y la forma iterativa del método pu
 La forma para varias ecuaciones se deriva en forma idéntica, pero a partir de la serie de Taylor para varias variables.
 Escribiendo esta ecuación en forma matricial: """
 st.latex(r"""
-         \begin{}
+        
                   \centerdot Edad de Piedra \\
                   \centerdot Edad del Cobre \\
                   \centerdot  Edad del Bronce \\
-                  \centerdot  Edad del Hierro \\
-         \end{}
+                  \centerdot  Edad del Hierro 
+  
          """)
 
