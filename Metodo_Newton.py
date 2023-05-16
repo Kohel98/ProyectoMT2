fe6d17457154602a840ac385b621be03650d6a63
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-15 19:12:36 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -5,3 +5,8 @@ import numpy as np
 
 
 st.header(":blue[Metodo de Newton]")
+"""
+Para poder dar una lista de opciones podemos hacer uso de st.selectbox la cual
+nos ayudara a poder dar una lista de valores para elegir. Para ello pasamos como
+argumentos la etiqueta(string), una tupla con las opciones a elegir.
+"""
