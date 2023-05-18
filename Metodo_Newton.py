1222d999f3120690d39e0f0ab2c28bbe893ed891
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-18 17:07:25 -0600

    Update and rename Metodo_Newton.py to Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -69,31 +69,11 @@ Escribiendo esta ecuación en forma matricial:
 
 """
 
-st.markdown("""
-1. List item one.
-+
-List item one continued with a second paragraph followed by an
-Indented block.
-+
-.................
-$ ls *.sh
-$ mv *.sh ~/tmp
-.................
-+
-List item continued with a third paragraph.
 
-2. List item two continued with an open block.
++ Para el sistema (1.1) se tiene: $F(X)=0$
+
 +
---
-This paragraph is part of the preceding list item.
 
-a. This list is nested and does not require explicit item
-continuation.
 +
-This paragraph is part of the preceding list item.
 
-b. List item b.
-
-This paragraph belongs to item two of the outer list.
---
 """)
