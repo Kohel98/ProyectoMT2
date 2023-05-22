6edbf1c4e5393e838134e0874a396fe7e6cdb57c
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-22 13:04:19 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -12,14 +12,10 @@ with st.sidebar:
          )
          
          if selected == "Teoria":
-                  
-         
-     
-st.title(":blue[Metodo de Newton(-Raphson)]")
-
-st.header("Teorema")
-"""
-El metodo de Newton es una alternativa del metodo del punto 
+                  st.title(":blue[Metodo de Newton(-Raphson)]")
+                  st.header("Teorema")
+                  """
+                  El metodo de Newton es una alternativa del metodo del punto 
 fijo ya que la gran desventaja de este se consiste en obtener ecuaciones $g_i$ que converja 
 en el intervalo de solucion y dicha búsqueda puede representar un esfuerzo de cálculo considerable, en lo unico en que se parecen en estos dos metodos es 
 la generalización multivariable del método de Newtoon para una sola variable.
