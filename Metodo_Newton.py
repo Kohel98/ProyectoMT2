f1698ffe57874462310e8f850ebec57cf4056293
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-22 14:14:19 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -5,3 +5,10 @@ import numpy as np
 add_selectbox = st.sidebar.selectbox(
          " ",("Metodo de Newton", "Metodo de Cuasi-Newton")
 )
+
+
+with st.sidebar:
+         st.radio(
+                  "elige",
+                  ("1","2")
+         )
