f8a9eca23dadea93dc6dcec775dc72368fb54625
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-22 14:17:54 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -5,7 +5,7 @@ import numpy as np
 add_selectbox = st.sidebar.selectbox(
          " ",("Metodo de Newton", "Metodo de Cuasi-Newton")
 )
-with st.sidebar.selectbox[1] : 
+with st.sidebar.selectbox : 
           st.title(":blue[Metodo de Newton(-Raphson)]")
 
 
