c32f10f52203394cdcfc013ebec72b1a286000c8
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-22 14:12:14 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -3,5 +3,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 add_selectbox = st.sidebar.selectbox(
-         "como empezar",("Definicion", "Ejemplo", "Aplicacion")
+         " ",("Metodo de Newton", "Metodo de Cuasi-Newton")
 )
