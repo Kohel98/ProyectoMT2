d9597a96ed73a2349f9451b618f95b2625d80cac
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-22 14:32:33 -0600

    Update Metodo_Newton.py

--- Metodo_Newton.py
+++ Metodo_Newton.py
@@ -11,12 +11,12 @@ with tab1:
          fijo ya que la gran desventaja de este se consiste en obtener ecuaciones $g_i$ que converja 
          en el intervalo de solucion y dicha búsqueda puede representar un esfuerzo de cálculo considerable, en lo unico en que se parecen en estos dos metodos es 
          la generalización multivariable del método de Newtoon para una sola variable.
-                  Este metodo solo sirve para los sistemas de ecuaciones no lineales, este requiere algunos conceptos previos, los cuales se presentan a continuación.
-                  La derivada, cuando se trabaja con funciones con varias variables, se emplean las derivadas parciales. La generalización de derivada para sistemas de funciones 
-                  de varias variables es la matriz jacobiana.
-                  :red[**Definición**]. Matriz jacobiana: Sean $f_i (x_1, x_2, ..., x_n)$ con $ 1 \leq i \leq n $, funciones con n variables $(x_i)$ independientes. La matriz jacobiana $J(x_1, x_2,..., x_n)$
-                  está dada por las derivadas parciales de cada una de las funciones con respecto a cada una de las variables: 
-                  """
+         Este metodo solo sirve para los sistemas de ecuaciones no lineales, este requiere algunos conceptos previos, los cuales se presentan a continuación.
+         La derivada, cuando se trabaja con funciones con varias variables, se emplean las derivadas parciales. La generalización de derivada para sistemas de funciones 
+         de varias variables es la matriz jacobiana.
+         :red[**Definición**]. Matriz jacobiana: Sean $f_i (x_1, x_2, ..., x_n)$ con $ 1 \leq i \leq n $, funciones con n variables $(x_i)$ independientes. La matriz jacobiana $J(x_1, x_2,..., x_n)$
+         está dada por las derivadas parciales de cada una de las funciones con respecto a cada una de las variables: 
+         """
          "Matriz Jacobiana (J):"
          st.latex(r"""
                   \begin{bmatrix}
