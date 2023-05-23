5aa6a7c1bcbc4f69bf8312799c46ea9f39eab29f
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-23 12:45:53 -0600

    Teoria Descenso_de_gradiente p1

--- pages/Descenso_de_gradiente.py
+++ pages/Descenso_de_gradiente.py
@@ -4,3 +4,17 @@ import numpy as np
 tab1, tab2, tab3 = st.tabs(["Definiciones","Ejemplos","Aplicaciones"])
 with tab1:
          st.title(":blue[Descenso de la Gradiente]")
+         """
+         Gradient Descent es conocido como uno de los algoritmos de optimización más utilizados
+         para entrenar modelos de aprendizaje automático mediante la minimización de errores 
+         entre los resultados reales y esperados.
+         
+         Además, el descenso de gradiente también se usa para entrenar redes neuronales.
+         
+         El :red[objetivo principal] del descenso de gradiente es minimizar la función convexa 
+         mediante la iteración de actualizaciones de parámetros. 
+         
+         Una vez que se optimizan estos modelos de aprendizaje automático,
+         estos modelos se pueden usar como herramientas poderosas para la inteligencia artificial 
+         y varias aplicaciones informáticas.
+         """
