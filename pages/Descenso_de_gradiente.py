88d0218329b679abd38a46a7d10636d283a841d2
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-23 12:35:35 -0600

    DEscenso pt1

---
+++ pages/Descenso_de_gradiente.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+tab1, tab2, tab3 = st.tabs(["Definiciones","Ejemplos","Aplicaciones"])
+with tab1:
+         st.title(":blue[Descenso de la Gradiente]")
