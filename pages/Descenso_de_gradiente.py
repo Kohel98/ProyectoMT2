83100fdef4f50e5aa4a847f0e1daf4d1c13abf92
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-23 22:12:06 -0600

    l

--- pages/Descenso_de_gradiente.py
+++ pages/Descenso_de_gradiente.py
@@ -27,28 +27,20 @@ with tab1:
             
          Su procedimiemto consiste en  usar un algoritmo de descenso de gradiente 
          en minimizar la función de costo usando la iteración. 
-            
-        Para lograr este objetivo, realiza dos pasos iterativamente:
-            +Calcula la derivada de primer orden de la función para calcular el gradiente o pendiente de esa función.
-            
-            +Alejarse de la dirección del gradiente, lo que significa que la pendiente aumentó desde el punto actual en alfa veces, 
-            donde Alfa se define como Tasa de aprendizaje. 
-            
-            Es un parámetro de ajuste en el proceso de optimización que ayuda 
-            a decidir el lapso del tiempo de los pasos.
-            Un ejemplo seria cuando se grafica una gradiente negativa (pasando las coordenadas del nuevo punto) 
-            con un proceso iterativo hasta encontrar el minimo local.
-            
-            Estos pasos se denomina como **taza de aprendizaje**. \\
-                 Con una tasa de aprendizaje alta podemos cubrir más terreno en cada paso, pero corremos el riesgo de 
-                 sobrepasar el punto más bajo ya que la pendiente de la colina cambia constantemente.
-                 Con una tasa de aprendizaje muy baja, podemos movernos con confianza en la dirección del gradiente 
-                 negativo ya que lo estamos recalculando con tanta frecuencia. Una tasa de aprendizaje baja es más precisa, 
-                 pero calcular el gradiente requiere mucho tiempo, por lo que nos llevará mucho tiempo llegar al fondo.
+         Para lograr este objetivo, realiza dos pasos iterativamente:
+         + Calcula la derivada de primer orden de la función para calcular el gradiente o pendiente de esa función.
+         + Alejarse de la dirección del gradiente, lo que significa que la pendiente aumentó desde el punto actual en alfa veces, donde Alfa se define como Tasa de aprendizaje. 
+         Es un parámetro de ajuste en el proceso de optimización que ayuda a decidir el lapso del tiempo de los pasos. Un ejemplo seria cuando se grafica una gradiente negativa (pasando las coordenadas del nuevo punto) con un proceso iterativo hasta encontrar el minimo local.
+         Estos pasos se denomina como :black[**taza de aprendizaje**]. 
+         Con una tasa de aprendizaje alta podemos cubrir más terreno en cada paso, pero corremos el riesgo de 
+         sobrepasar el punto más bajo ya que la pendiente de la colina cambia constantemente.
+         Con una tasa de aprendizaje muy baja, podemos movernos con confianza en la dirección del gradiente 
+         negativo ya que lo estamos recalculando con tanta frecuencia. Una tasa de aprendizaje baja es más precisa, 
+         pero calcular el gradiente requiere mucho tiempo, por lo que nos llevará mucho tiempo llegar al fondo.
                  
-            :red[**Función de costo**] \\
-                Con esta por medio de la función de pérdida determinamos "Qué tan bueno" es nuestro modelo para hacer 
-                predicciones para un conjunto dado de parámetros. La función de costo tiene su proria curva y sus gradientes.
-                La pendiente de esta curvatura nos mostrara cómo actualizar nuestros parámetros para que el modelo sea más preciso.
+        :red[**Función de costo**] 
+        Con esta por medio de la función de pérdida determinamos "Qué tan bueno" es nuestro modelo para hacer 
+        predicciones para un conjunto dado de parámetros. La función de costo tiene su proria curva y sus gradientes.
+        La pendiente de esta curvatura nos mostrara cómo actualizar nuestros parámetros para que el modelo sea más preciso.
                 
          """
