10386b1227089ca03785cd216d8bb857d34ae448
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-24 14:16:05 -0600

    Cargando Datos p2

--- pages/Descenso_de_gradiente.py
+++ pages/Descenso_de_gradiente.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pyplot as plt
 import seaborn as sn
 ##Se carga archivos excel para el ejercicio 
 
