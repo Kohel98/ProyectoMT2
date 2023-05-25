cd8d4bdd50f0ba183ada76bc89fe1bf1348a58b8
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-25 01:55:14 -0600

    aver3

--- pages/Descenso_de_gradient.py
+++ pages/Descenso_de_gradient.py
@@ -62,8 +62,7 @@ with tab3:
         st.write("Enter Note")
         stock_ticker_input = st.text_input('Stock', key='ticker')
         note_input = st.text_input('Note', key='note')
-    
-    submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Submit")
     
     if submitted:
         st.write("Note", note_input, "stock_ticker", stock_ticker_input)
